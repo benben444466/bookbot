7f87f0f081bd11a8f6b261c92ffb69aeed0ec7ab
benben444466 <pwc123451@gmail.com>
2025-10-20 15:10:26 -0400

    so mad, I didn't even know what problem I had... the code was all correct but for some reason it didn't work. I had to open up the answer key which costed me all the points for the lesson. I rewrote the same stuff over again and it worked...

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from stats import *
+from stats import (
+    get_word_count,
+    get_char_count,
+    get_sorted_char_dict
+)
 
 def get_book_text(filepath) -> str:
     with open(filepath) as f:
@@ -6,10 +10,22 @@ def get_book_text(filepath) -> str:
     return file_contents
 
 def main():
+    print("============ BOOKBOT ============")
     text_str = get_book_text("books/frankenstein.txt")
-    num_words = get_word_count(text_str)
-    print(f"Found {num_words} total words")
-    print(get_char_count(text_str))    
 
-if __name__ == "__main__":
-    main()
+    print("Analyzing book found at books/frankenstein.txt...")
+    num_words = get_word_count(text_str)
+
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+
+    unsorted_dict = get_char_count(text_str)
+    print("--------- Character Count -------")
+    sorted_list_of_dict = get_sorted_char_dict(unsorted_dict)
+    for entry_dict in sorted_list_of_dict:
+        if entry_dict["name"].isalpha():
+            name, num = entry_dict["name"], entry_dict["num"]
+            print(f"{name}: {num}")
+    print("============= END ===============")
+
+main()
--- stats.py
+++ stats.py
@@ -7,9 +7,19 @@ def get_char_count(content) -> dict:
 #Need to check if it is a letter than change to lowercase.
     for word in words:
         for letter in word:
-            #print(letter + "0")
             if letter.lower() in char_dict:
                 char_dict[letter.lower()] += 1
             else:
                 char_dict[letter.lower()] = 1
     return char_dict
+
+def sort_on(d):
+    return d["num"]
+
+def get_sorted_char_dict(char_dict) -> list[dict]:
+    result = []
+    for key, value in char_dict.items():
+        entry = {"name": str(key), "num": int(value)}
+        result.append(entry)
+    result.sort(reverse=True, key=sort_on)
+    return result
